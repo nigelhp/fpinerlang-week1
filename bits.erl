593a5c0b04841e11fe2c8ff225d70a80642c6281
Nigel Perkins <nigelhp@yahoo.co.uk>
2017-02-25 15:39:32 +0000

    Introduce eunit unit-tests

--- bits.erl
+++ bits.erl
@@ -7,39 +7,44 @@
 
 -module(bits).
 -export([bits/1,trbits/1]).
+-include_lib("eunit/include/eunit.hrl").
 
+% direct recursive
 bits(0) ->
     0;
 bits(N) when N>0 ->
     N band 1 + bits(N bsr 1).
 
-% examples:
-% bits:bits(0). == 0
-% bits:bits(1). == 1
-% bits:bits(2). == 1
-% bits:bits(3). == 2
-% bits:bits(4). == 1
-% bits:bits(5). == 2
-% bits:bits(6). == 2
-% bits:bits(7). == 3
-% bits:bits(8). == 1
 
+% tail recursive
 trbits(N) ->
     trbits(N,0).
-
 
 trbits(0,S) ->
     S;
 trbits(N,S) when N>0 ->
     trbits(N bsr 1,S+(N band 1)).
 
-% examples:
-% bits:trbits(0). == 0
-% bits:trbits(1). == 1
-% bits:trbits(2). == 1
-% bits:trbits(3). == 2
-% bits:trbits(4). == 1
-% bits:trbits(5). == 2
-% bits:trbits(6). == 2
-% bits:trbits(7). == 3
-% bits:trbits(8). == 1
+
+% execute unit-tests from erl shell with: 'bits:test().'
+bits_test_() ->
+    [?_assert(bits(0) =:= 0),
+     ?_assert(bits(1) =:= 1),
+     ?_assert(bits(2) =:= 1),
+     ?_assert(bits(3) =:= 2),
+     ?_assert(bits(4) =:= 1),
+     ?_assert(bits(5) =:= 2),
+     ?_assert(bits(6) =:= 2),
+     ?_assert(bits(7) =:= 3),
+     ?_assert(bits(8) =:= 1)].
+
+trbits_test_() ->
+    [?_assert(trbits(0) =:= 0),
+     ?_assert(trbits(1) =:= 1),
+     ?_assert(trbits(2) =:= 1),
+     ?_assert(trbits(3) =:= 2),
+     ?_assert(trbits(4) =:= 1),
+     ?_assert(trbits(5) =:= 2),
+     ?_assert(trbits(6) =:= 2),
+     ?_assert(trbits(7) =:= 3),
+     ?_assert(trbits(8) =:= 1)].
