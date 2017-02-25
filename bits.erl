6bf32d897618f40f6b7eb529612de988e5018e4e
Nigel Perkins <nigelhp@yahoo.co.uk>
2017-02-25 12:35:39 +0000

    Bits via tail recursion

--- bits.erl
+++ bits.erl
@@ -6,7 +6,7 @@
 % Which do you think is better?  Why?
 
 -module(bits).
--export([bits/1]).
+-export([bits/1,trbits/1]).
 
 bits(0) ->
     0;
@@ -23,3 +23,23 @@ bits(N) when N>0 ->
 % bits:bits(6). == 2
 % bits:bits(7). == 3
 % bits:bits(8). == 1
+
+trbits(N) ->
+    trbits(N,0).
+
+
+trbits(0,S) ->
+    S;
+trbits(N,S) when N>0 ->
+    trbits(N bsr 1,S+(N band 1)).
+
+% examples:
+% bits:trbits(0). == 0
+% bits:trbits(1). == 1
+% bits:trbits(2). == 1
+% bits:trbits(3). == 2
+% bits:trbits(4). == 1
+% bits:trbits(5). == 2
+% bits:trbits(6). == 2
+% bits:trbits(7). == 3
+% bits:trbits(8). == 1
